2b5cf9ae69df123e61a4b0674e879a757f6dfa37
Jiropas <jiropas06@gmail.com>
2024-08-27 14:33:16 +0700

    test student list

--- src/test/java/ku/cs/models/StudentListTest.java
+++ src/test/java/ku/cs/models/StudentListTest.java
@@ -20,10 +20,45 @@ public class StudentListTest {
     void testAddNewStudent(){
         Student s1 = new Student("6610450862", "Jiropas");
         studentList.addNewStudent("6610450862", "Jiropas");
-        assertEquals("")
 
-
-
+        Student s = studentList.findStudentById("6610450862");
+        assertEquals("Jiropas", s.getName());
     }
 
+    @Test
+    @DisplayName("Find Student Test")
+    void testFindStudent(){
+        studentList.addNewStudent("6610450001", "mo");
+        studentList.addNewStudent("6610450002", "dew");
+        studentList.addNewStudent("6610450003", "plai");
+
+        Student s = studentList.findStudentById("6610450001");
+        assertEquals("mo", s.getName());
+    }
+
+    @Test
+    @DisplayName("Give score test")
+    void testGiveScore(){
+        studentList.addNewStudent("6610450001", "mo");
+        studentList.addNewStudent("6610450002", "dew");
+        studentList.addNewStudent("6610450003", "plai");
+
+        studentList.giveScoreToId("6610450001", 100);
+        double expected = 100;
+        double actual = studentList.findStudentById("6610450001").getScore();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("View grade test")
+    void testViewScore(){
+        studentList.addNewStudent("6610450001", "mo");
+        studentList.addNewStudent("6610450002", "non");
+        studentList.addNewStudent("6610450003", "plai");
+
+        studentList.giveScoreToId("6610450001", 100);
+        String expected = "A";
+        String actual = studentList.viewGradeOfId("6610450001");
+        assertEquals(expected, actual);
+    }
 }
